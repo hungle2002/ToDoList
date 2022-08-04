5e3b93983ef5958084ff2d5b0b23b0fd17e17c81
lehung <lehung16082002@gmail.com>
2022-08-04 22:51:53 +0700

    modify smth

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require("express")
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose")
-var _ = require('lodash');
+// const express = require("express")
+// const bodyParser = require("body-parser");
+// const mongoose = require("mongoose")
+// var _ = require('lodash');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended : true}));
